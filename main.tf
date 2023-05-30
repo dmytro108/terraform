b0c7666f677f302483ab211c96bf09c8d24b7fb5
dmytro108 <dmitry.fedunets@gmail.com>
2023-05-30 01:21:39 +0100

    fix provider

--- main.tf
+++ main.tf
@@ -1,9 +1,15 @@
 terraform{
     required_version = ">= 1.0"
     backend "local" {}
+    required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
 }
 
-provider "aws"{
+provider "aws" {
     region = var.region
 }
 
