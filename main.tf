249c3b294e0ee8a2564109aa5ff1823c0effc649
dmytro108 <dmitry.fedunets@gmail.com>
2023-05-31 00:57:56 +0100

    vpc module fix - cidr, name

--- main.tf
+++ main.tf
@@ -18,18 +18,18 @@ provider "aws" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
+  name = "vpc_epam"
+  cidr = "192.168.0.0/16"
 
   azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["192.168.1.0/24"]
+  public_subnets  = ["192.168.10.0/24"]
 
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
 
   tags = {
     Terraform = "true"
-    Environment = "dev"
+    Environment = "epam"
   }
 }
