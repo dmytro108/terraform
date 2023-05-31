a4c8bac58c401116574864d6ab1914fe6f498bab
dmytro108 <dmitry.fedunets@gmail.com>
2023-05-31 03:33:21 +0100

    fix variables

--- main.tf
+++ main.tf
@@ -12,9 +12,9 @@ terraform{
 
 //AWS provider, authentification with aws cli file
 provider "aws" {
-    region = var.region
-    shared_credentials_files = ["$HOME/.aws/credentials"]
-    profile                 = "default"
+    region                   = var.region
+    shared_credentials_files = [var.credential_path]
+    profile                  = var.aws_cli_profile
 }
 
 // Create VPC 
@@ -22,29 +22,31 @@ provider "aws" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "vpc_epam"
-  cidr = "192.168.0.0/16"
-
-  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
-  private_subnets = ["192.168.1.0/24"]
-  public_subnets  = ["192.168.10.0/24"]
+  cidr            = var.vpc_cidr
+  azs             = var.availability_zones
+  private_subnets = var.priv_net_cidrs
+  public_subnets  = var.pub_net_cidrs
 
   enable_nat_gateway = false
   enable_vpn_gateway = false
 
   tags = {
-    Terraform = "true"
-    Environment = "epam"
+    "Name"        = var.vpc_name
+    "Terraform"   = "true"
+    "Environment" = var.env_name
   }
 }
 
 //Create 2 EC2 instances in the private subnet
 resource "aws_instance" "servers" {
-    count = 2
-    ami = "ami-0e1c5be2aa956338b" //AWS Linux 2023 x86_64
-    instance_type = "t3.micro"
-    subnet_id = module.vpc.private_subnets[0]
+    count         = var.serv_num
+    ami           = var.ami_id
+    instance_type = var.ec2_type
+    subnet_id     = module.vpc.private_subnets[0]
+
     tags = {
-        "Name" = "server-${count.index}"
-    }
+        "Name"        = "server-${count.index}"
+        "Terraform"   = "true"
+        "Environment" = var.env_name
+   }
 }
--- variables.tf
+++ variables.tf
@@ -7,11 +7,66 @@ variable "region"{
 variable "vpc_name"{
     description = "VPC name tag"
     type = string
-    default = "VPC Noname"
+    default = "Noname"
+}
+
+variable "env_name"{
+    description = "Environment tag"
+    type = string
+    default = "Unknown"
 }
 
 variable "vpc_cidr" {
     description = "VPC CIDR"
     type = string
     default = "10.0.0.0/16"  
+}
+
+variable "pub_net_cidrs" {
+    description = "Public subnet CIDR"
+    type = tuple([ string ])
+    default = ["10.0.0.0/24"]  
+}
+
+variable "priv_net_cidrs" {
+    description = "Privat subnet CIDR"
+    type = tuple([ string ])
+    default = ["10.0.10.0/24"]  
+}
+
+variable "credential_path" {
+    description = "Path to credential file"
+    type = string
+    default = "$HOME/.aws/credentials"  
+}
+
+variable "aws_cli_profile" {
+    description = "Profile name in AWS CLI credential and config file"
+    type = string
+    default = "default"  
+}
+
+variable "ami_id" {
+    description = "AWS ami id"
+    type = string
+    default = "ami-0e1c5be2aa956338b" //AWS Linux 2023 x86_64  
+}
+
+variable "ec2_type" {
+    description = "AWS instance type"
+    type = string
+    default = "t3.micro" // Free tier
+}
+
+variable "availability_zones" {
+    description = "azs"
+    type = tuple([ string ])
+    default = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]  
+}
+
+variable "serv_num" {
+    description = "Quantity of EC2 instances in privat subnet"
+    type = number
+    default = 0
+  
 }
