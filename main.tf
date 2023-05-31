9245928251cadcaf855089a9889243fa1fae5db2
dmytro108 <dmitry.fedunets@gmail.com>
2023-05-31 01:46:58 +0100

    ec2 instances

--- main.tf
+++ main.tf
@@ -1,20 +1,24 @@
+// Terraform init with latest aws provider
 terraform{
     required_version = ">= 1.0"
     backend "local" {}
     required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 5.0"
+        }
     }
-  }
 }
 
+//AWS provider, authentification with aws cli file
 provider "aws" {
     region = var.region
     shared_credentials_files = ["$HOME/.aws/credentials"]
     profile                 = "default"
 }
 
+// Create VPC 
+// Two subnets public and private, no NAT
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -32,4 +36,15 @@ module "vpc" {
     Terraform = "true"
     Environment = "epam"
   }
+}
+
+//Create 2 EC2 instances in the private subnet
+resource "aws_instance" "servers" {
+    count = 2
+    ami = "ami-0e1c5be2aa956338b" //AWS Linux 2023 x86_64
+    instance_type = "t3.micro"
+    subnet_id = module.vpc.private_subnets[0]
+    tags = {
+        "Name" = "server-${count.index}"
+    }
 }
